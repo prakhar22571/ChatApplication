c30db6017285573e3909058443cc7e2b12cc43c2
prakhar22571 <prakhar.raicic@gmail.com>
2024-05-18 23:45:50 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,9 @@ app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
 	connectToMongoDB();
