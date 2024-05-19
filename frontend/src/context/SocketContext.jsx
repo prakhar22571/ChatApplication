ebe0faefe2de85a6a8e937a96a6cd511318023e8
prakhar22571 <prakhar.raicic@gmail.com>
2024-05-19 11:30:49 +0530

    socket.io URL added

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chatterbox-s1bc.onrender.com", {
         query: {
           userId: authUser._id,
         },
